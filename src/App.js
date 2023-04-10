3185e0a52516f679e61d5179c8455b5d36eca37f
MarliDesign <113238574+MarliDesign@users.noreply.github.com>
2023-04-10 19:14:21 +0200

    added footer element

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import logo from "./logo.svg";
 import "./App.css";
 import WeatherSearch from "./WeatherSearch";
+import Footer from "./Footer.js";
 
 function App() {
   return (
     <div className="App">
       <h1>Weather App</h1>
       <WeatherSearch />
+      <Footer />
     </div>
   );
 }
---
+++ src/Footer.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <div>
+      <p>
+        <a href="https://github.com/MarliDesign/shecodes-weather-app-react">
+          open source code
+        </a>
+        by Linda
+      </p>
+    </div>
+  );
+}
