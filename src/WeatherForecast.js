4cf0a99ce23307efa94a9acc37e8b0f72c05f353
MarliDesign <113238574+MarliDesign@users.noreply.github.com>
2023-06-10 13:04:18 -0400

    added lon & lat values to weather forecast

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,15 +1,29 @@
 import React from "react";
 import "./WeatherForecast.css";
 import WeatherIcon from "./WeatherIcon";
+import axios from "axios";
 
 export default function WeatherForecast(props) {
+  function handleResponse(response) {
+    console.log(response.data);
+  }
+
+  let apiKey = "ebef9ca4a8de66ed586fac628fade056";
+  let latitude = props.cooridnates.lat;
+  let longitude = props.coordinates.lon;
+  let apiUrl = `https://api.openweathermap.org/data/3.0/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
+
+  axios.get(apiUrl).then(handleResponse);
   return (
     <div className="WeatherForecast">
       <div className="row">
         <div className="col">
           <div className="WeatherForecast-day">Thu</div>
           <div>
-            <WeatherIcon />
+            <WeatherIcon
+              code="01d"
+              size={26}
+            />
           </div>
           <div className="WeatherForecast-temperatures">
             <span className="WeatherForecast-temperature-max">19°</span>
